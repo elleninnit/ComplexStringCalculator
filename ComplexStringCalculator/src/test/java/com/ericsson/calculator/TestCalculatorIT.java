26247dbbdcf0a1293094378df9fae2152df2cba9
Ellen Thomas <eleohlt@IE00203822.ericsson.se>
2018-10-09 09:26:14 +0100

    Updated pom to create docker image for integration test, updated integration test to use chromedriver for linux. Added dockerfile.

--- ComplexStringCalculator/src/test/java/com/ericsson/calculator/TestCalculatorIT.java
+++ ComplexStringCalculator/src/test/java/com/ericsson/calculator/TestCalculatorIT.java
@@ -11,22 +11,30 @@ public class TestCalculatorIT {
     
     private WebDriver driver;
 
-    private static final String INPUT = "input";
-    private static final String CALCULATE_BUTTON = "calculateButton";
-    private static final String RESULT = "result";
+    private final String INPUT = "input";
+    private final String CALCULATE_BUTTON = "calculateButton";
+    private final String RESULT = "result";
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eleohlt\\Documents\\Git\\ComplexStringCalculator\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/mnt/eleohlt/home/Documents/Git/ComplexStringCalculator/ComplexStringCalculator/chromedriver");
         driver = new ChromeDriver();
-        driver.get("http://localhost:8080");
+        driver.get("http://localhost:8080/");
     }
+    
+    ///mnt/eleohlt/home/Documents/Git/ComplexStringCalculator/ComplexStringCalculator/chromedriver
+    //C:\\Users\\eleohlt\\Documents\\Git\\ComplexStringCalculator\\ComplexStringCalculator\\chromedriver.exe
 
     @Test
     public void testValidInput() {
         String input = "21+18/3+4/4-3+6-3";
         driver.findElement(By.id(INPUT)).sendKeys(input);
         driver.findElement(By.id(CALCULATE_BUTTON)).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         assertEquals("28", driver.findElement(By.id(RESULT)).getText());
     }
     
@@ -39,7 +47,8 @@ public class TestCalculatorIT {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
         driver.quit();
     }
     
